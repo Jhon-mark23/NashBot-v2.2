7026ad52682b2118e7d2400c7abaac6223e5a750
Jhon-mark23 <123864853+Jhon-mark23@users.noreply.github.com>
2024-09-02 09:02:35 +0800

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = {
-    name: 'ai',
+    name: 'juv',
     description: 'Interact with GPT-3.5 Turbo',
     cooldown: 3,
     nashPrefix: false,
@@ -24,7 +24,7 @@ module.exports = {
             }
 
             api.sendMessage(
-                `🤖 AI Response\n━━━━━━━━━━━━━━━━━━━\n${result}`,
+                `🧋✨|GEO-CHAT BOT\n━━━━━━━━━━━━━━━━━━━\n${result}`,
                 event.threadID,
                 event.messageID
             );
